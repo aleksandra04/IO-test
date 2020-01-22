8d99197304520e207ef9a05ea0ac8f570f8d56da
Alexandra Boyko <Alexboyko04@gmail.com>
2020-01-22 21:25:08 +0200

    Updates

---
+++ precache-manifest.3e47eb11951f1c2e53f505db483ae606.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ce38200d67a64b4811baffafd95dfd",
+    "url": "/IO-test/index.html"
+  },
+  {
+    "revision": "e62662a03fd5e61fe7b3",
+    "url": "/IO-test/static/css/main.998ec721.chunk.css"
+  },
+  {
+    "revision": "d042a78bc99a59ac5afc",
+    "url": "/IO-test/static/js/2.7c0272e0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/IO-test/static/js/2.7c0272e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e62662a03fd5e61fe7b3",
+    "url": "/IO-test/static/js/main.f0a72e3d.chunk.js"
+  },
+  {
+    "revision": "47473d228f254ab92f35",
+    "url": "/IO-test/static/js/runtime-main.b5c366d4.js"
+  },
+  {
+    "revision": "4a0aa783d4caa5ce5c6d59569e032c05",
+    "url": "/IO-test/static/media/arrow.4a0aa783.svg"
+  },
+  {
+    "revision": "94ddd74c046504039ad441c418b3fc46",
+    "url": "/IO-test/static/media/background-dark.94ddd74c.svg"
+  }
+]);
